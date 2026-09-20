231f3ca50704ec90b2a099f1cf537b27fba65792
vasubansal-git <vasubansal8907@gmail.com>
2026-09-20 20:33:58 +0530

    Add streaming files

--- llm-apis/api-fundamentals/python-requests/downloadingRequests.py
+++ llm-apis/api-fundamentals/python-requests/downloadingRequests.py
@@ -1,4 +1,5 @@
 import requests
+from tqdm import tqdm
 from PIL import Image
 from io import BytesIO
 
@@ -17,3 +18,17 @@ r = requests.get(url)
 fp = open('winrar.exe', 'wb')
 fp.write(r.content)
 fp.close()
+
+# streaming files --------------------------------------------------------------------
+
+url = "https://www.python.org/ftp/python/pymanager/python-manager-26.3.msix"
+r = requests.get(url, stream=True)
+totalExpectedBytes = int(r.headers["Content-Length"])
+bytesReceived = 0
+progress_bar = tqdm(total=totalExpectedBytes, unit='iB', unit_scale=True)
+with open("python.exe", "wb") as f:
+    for chunk in r.iter_content(chunk_size=128):
+        progress_bar.update(128)
+        f.write(chunk)
+        bytesReceived += 128
+progress_bar.close()
