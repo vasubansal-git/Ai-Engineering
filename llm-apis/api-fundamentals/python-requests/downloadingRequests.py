d780fdf02d4adcbef375f76db972c1c78db8c917
vasubansal-git <vasubansal8907@gmail.com>
2026-09-20 13:15:05 +0530

    Add downloading requests

--- llm-apis/api-fundamentals/python-requests/downloadingRequests.py
+++ llm-apis/api-fundamentals/python-requests/downloadingRequests.py
@@ -2,8 +2,18 @@ import requests
 from PIL import Image
 from io import BytesIO
 
+# downloading request for images ---------------------------------------------------
+
 r = requests.get('https://images.pexels.com/photos/30994370/pexels-photo-30994370.jpeg?cs=srgb&dl=pexels-optical-chemist-340351297-30994370.jpg&fm=jpg')
 i = Image.open(BytesIO(r.content))
 fp = open("img.jpg", "wb")
 i.save(fp)
+fp.close()
+
+# downloading request for files like (exe, mp3, wav etc) ------------------------------------
+
+url = "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-723.exe"
+r = requests.get(url)
+fp = open('winrar.exe', 'wb')
+fp.write(r.content)
 fp.close()
